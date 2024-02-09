59c46c1541a032e6491ac8cf32ff5672d3f0dcf4
Ridansan <ridanpc@gmail.com>
2024-02-09 07:22:02 +0700

    test : update

---
+++ 5/Exercise5.js
@@ -0,0 +1,27 @@
+/*let ress = []
+function HLA = [12, 5, 23, 18, 4, 45, 32];
+for (i = 0; i <= 1; i++);
+    if()
+console.log(HLA[0]);
+
+*/
+
+/*let arr = ["apple", "banana", "cherry", "date"];
+function fruit(arr){
+
+};
+console.log(arr);
+*/
+
+/*let word = "Hello Word";
+let ress = [word];
+console.log(ress);
+*/
+
+/*let arr1 = [1, 2, 3];
+let arr2 = [3, 2, 1];
+function sum(arr1, arr2){
+}
+console.log(arr1)
+*/
+
